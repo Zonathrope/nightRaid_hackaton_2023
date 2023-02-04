ddf720d520f3fdd7bc6e8a012d2e63c8177a711a
oleksandr.kachanov <oleksandr.kachanov@codex-soft.com>
2023-02-04 12:32:20 +0200

    update project structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <header>
+        <img className={styles.logo}>
+
+        </img>
+        <nav>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Header
